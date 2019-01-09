5f18a88f71c40a2d39cb7a669a51145cdf8fc1ab
Michael Gawlik <michaelgawlik1983@gmail.com>
2019-01-09 05:23:15 +0100

    added `iid` parameter to `com_interface` macro

--- idl/src/lib.rs
+++ idl/src/lib.rs
@@ -1,31 +1,45 @@
-#![recursion_limit="128"]
+#![recursion_limit = "128"]
 
 extern crate proc_macro;
 extern crate proc_macro2;
 
-use proc_macro2::{Ident, Span};
+use proc_macro2::{
+    Ident,
+    Span,
+};
 
 type TokenStream = proc_macro::TokenStream;
 
+use quote::quote;
 use syn::{
     parse_macro_input,
-    ItemStruct
+    AttributeArgs,
+    ItemStruct,
+    Lit,
+    Meta,
+    MetaNameValue,
+    NestedMeta,
 };
-use quote::quote;
 
 #[proc_macro_attribute]
-pub fn com_interface(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    //let attr = syn::parse::parse (input).unwrap();
+pub fn com_interface(
+    attr: TokenStream,
+    item: TokenStream,
+) -> TokenStream {
     let item = parse_macro_input!(item as ItemStruct);
+    let attr = parse_macro_input!(attr as AttributeArgs);
 
-    com_interface_impl(item).into()
+    com_interface_impl(attr, item).into()
 }
 
-fn com_interface_impl(item: ItemStruct) -> proc_macro2::TokenStream{
+fn com_interface_impl(
+    attr: AttributeArgs,
+    item: ItemStruct,
+) -> proc_macro2::TokenStream {
     let struct_name = item.ident;
-    let vtable_name = Ident::new(
-        &format!("{}_VTable", struct_name),
-        Span::call_site());
+    let vtable_name = Ident::new(&format!("{}_VTable", struct_name), Span::call_site());
+
+    let _iid = get_iid(&attr);
 
     let gen = quote! {
         struct #struct_name {
@@ -47,34 +61,84 @@ fn com_interface_impl(item: ItemStruct) -> proc_macro2::TokenStream{
     gen
 }
 
-#[test]
-fn test(){
-
-    let input: ItemStruct = syn::parse_quote!{
-        pub struct Foo;
-    };
-
-    let actual = com_interface_impl(input);
-
-    let expected = quote::quote! {
-        struct Foo {
-            vtable: *const Foo_VTable
-        }
-
-        impl ::safercom::ComInterface for Foo {
-            type VTable = Foo_VTable;
-
-            const IID: ::safercom::types::IID =
-                ::safercom::types::IID::new(0x0000000, 0x0000, 0x0000, [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
-
-            unsafe fn vtable(&self) -> *const Self::VTable {
-                self.vtable
+fn get_iid(args: &AttributeArgs) -> Result<String, &'static str> {
+    for meta in args {
+        if let NestedMeta::Meta(Meta::NameValue(MetaNameValue {
+            ident: name, lit, ..
+        })) = meta
+        {
+            if format!("{}", name) == "iid" {
+                if let Lit::Str(value) = lit {
+                    return Ok(value.value());
+                } else {
+                    return Err("value_not_a_string");
+                }
             }
         }
-    };
-
-    assert_eq!(
-        format!("{}", expected),
-        format!("{}", actual));
+    }
+    Err("No IID parameter")
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+    use quote::quote;
+    use syn::parse_quote;
+
+    #[test]
+    fn get_iid_success_test() {
+        let meta: NestedMeta = parse_quote!(iid = "d8f015c0-c278-11ce-a49e444553540000");
+
+        assert_eq!(
+            get_iid(&vec![meta]),
+            Ok("d8f015c0-c278-11ce-a49e444553540000".to_string())
+        );
+    }
+
+    #[test]
+    fn get_iid_failure_wrong_name_test() {
+        let meta: NestedMeta = parse_quote!(foo = "d8f015c0-c278-11ce-a49e444553540000");
+
+        assert_eq!(get_iid(&vec![meta]), Err("No IID parameter"));
+    }
+
+    #[test]
+    fn get_iid_failure_value_not_a_string_test() {
+        let meta: NestedMeta = parse_quote!(iid = 3454325);
+
+        assert_eq!(get_iid(&vec![meta]), Err("value_not_a_string"));
+    }
+
+    #[test]
+    fn com_interface_impl_test() {
+        let attr = {
+            let iid: NestedMeta = parse_quote!(iid = "d8f015c0-c278-11ce-a49e444553540000");
+            vec![iid]
+        };
+
+        let item: ItemStruct = syn::parse_quote! {
+            pub struct Foo;
+        };
+
+        let actual = com_interface_impl(attr, item);
+
+        let expected = quote::quote! {
+            struct Foo {
+                vtable: *const Foo_VTable
+            }
+
+            impl ::safercom::ComInterface for Foo {
+                type VTable = Foo_VTable;
+
+                const IID: ::safercom::types::IID =
+                    ::safercom::types::IID::new(0x0000000, 0x0000, 0x0000, [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+                unsafe fn vtable(&self) -> *const Self::VTable {
+                    self.vtable
+                }
+            }
+        };
+
+        assert_eq!(format!("{}", expected), format!("{}", actual));
+    }
+}
--- tests/com_interface.rs
+++ tests/com_interface.rs
@@ -5,7 +5,7 @@ extern crate safercom;
 
 use idl::com_interface;
 
-#[com_interface]
+#[com_interface(iid="11112222-3333-4444-55556666")]
 struct Foo;
 
 #[allow(non_camel_case_types)]
